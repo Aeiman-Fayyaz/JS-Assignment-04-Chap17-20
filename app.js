15be9291c9e981afcc3d30b3ab1ab96638e1bd54
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-02-17 00:45:21 +0500

    code update

---
+++ app.js
@@ -0,0 +1,60 @@
+// QUESTION 1
+// Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+
+// ANSWER: 1
+
+// let multiArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// document.write(multiArray);
+
+
+// QUESTION 2
+// Declare and initialize a multidimensional array
+// representing the following matrix:
+
+// ANSWER: 2
+
+// let multidimensionalArray = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
+
+// document.write(multidimensionalArray[0] + "<br>");
+// document.write(multidimensionalArray[1] + "<br>");
+// document.write(multidimensionalArray[2] + "<br>");
+
+// QUESTION 3
+// Write a program to print numeric counting from 1 to 10.
+
+// ANSWER: 3
+
+// for (let i = 1 ; i <= 10 ; i++) {
+//     document.write(i + "<br>");
+// }
+
+// QUESTION 4
+// Write a program to print multiplication table of any
+// number using for loop. Table number & length should be
+// taken as an input from user.
+
+// ANSWER: 4
+
+let userNumber = +prompt("Enter a number to show its multiplication table:");
+
+let userTableLength = +prompt("Enter length of multiplication table:");
+
+// QUESTION 5
+// ANSWER: 5
+
+// QUESTION 6
+// ANSWER: 6
+
+// QUESTION 7
+// ANSWER: 7
+
+// QUESTION 8
+// ANSWER: 8
+
+// QUESTION 9
+// ANSWER: 9
+
+// QUESTION 10
+// ANSWER: 10
