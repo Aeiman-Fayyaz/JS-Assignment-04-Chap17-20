699305e2859596b9f0df79d8049933ae841cf3a5
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-02-18 23:58:04 +0500

    code update

--- app.js
+++ app.js
@@ -93,11 +93,15 @@ for (let i = 1 ; i <= fruits.length ; i++){
 
 // ANSWER: 6
 
-document.write("<h3>Counting:</h3>" + "<br>");
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+
+/*document.write("<h3>Counting:</h3>" + "<br>");
 
 for (let i = 1 ; i <= 15 ; i++) {
     document.write(i + " ");
 }
+
+// b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
 
 document.write("<br>");
 document.write("<h3>Reverse Counting:</h3>" + "<br>");
@@ -106,6 +110,8 @@ for (let i = 10 ; i >= 1 ; i--){
     document.write(i + " ");
 }
 
+// c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+
 document.write("<br>");
 document.write("<h3>Even Numbers:</h3>" + "<br>");
 
@@ -113,14 +119,97 @@ for (let i = 0 ; i <= 20 ; i += 2){
     document.write(i + " ");
 }
 
+// d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+
+document.write("<br>");
+document.write("<h3>Odd Numbers:</h3>" + "<br>");
+
+for (let i = 1 ; i <= 15 ; i++ ){
+    console.log(i + " ");
+    document.write(i + " ");
+    i++;
+}
+
+// e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+
+document.write("<br>");
+document.write("<h3>Series:</h3>" + "<br>");
+
+for (let i = 2 ; i <= 20 ; i += 2){
+    document.write(i + "k ");
+}*/
+
+
 // QUESTION 7
+// You have an array
+// A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”]
+// Write a program to enable “search by user input” in an
+// array.
+// After searching, prompt the user whether the given item is
+// found in the list or not.
+
 // ANSWER: 7
 
+/*let ourBakery = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+let userWants = prompt("Welcome to Aeiman's Bakery \nWhat do you want to order?");
+userWants = userWants.toLowerCase();
+
+let flag = false;
+
+for (let i = 0 ; i < ourBakery.length ; i++){
+    if (userWants === ourBakery[i]){
+        flag = true;
+        break;
+    }
+}
+if(flag){
+    document.write(userWants + " is available");
+}
+else{
+    document.write("We are sorry. " + userWants + " is not available");
+}
+
+// if(userWants == ourBakery.length){
+//     flag = true;
+//     document.write(userWants + " " + "is available at" + " " + ourBakery)
+// }
+// else(flag == false)
+//     document.write("We are sorry" + " " + userWants + " " + "is not available at our bakery")*/
+
+
 // QUESTION 8
+// Write a program to identify the largest number in the
+// given array.
+// A = [24, 53, 78, 91, 12].
+
 // ANSWER: 8
+
+/*let numbers = [24, 53, 78, 91, 12];
+
+let largestNumber = numbers[0];
+
+for (numbers = 1 ; numbers < numbers.length ; numbers++){
+    if (numbers > largestNumber){
+        document.write(numbers + " is the largest number");
+    }
+   
+    
+}*/
 
 // QUESTION 9
 // ANSWER: 9
 
 // QUESTION 10
+// Write a program to print multiples of 5 ranging 1 to
+// 100.
+
 // ANSWER: 10
+
+/*document.write("<h3>Multiples of 5:</h3>" + "<br>");
+
+for (let i = 1 ; i <= 100 ; i++){
+    if (i % 5 === 0){
+        document.write(i + " , ");
+    }
+}*/
